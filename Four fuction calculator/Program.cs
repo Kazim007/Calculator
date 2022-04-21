ba9a1e81b3e50ace6829470cb403e53c55b957c7
Kazim <mkaxymkhan3@gmail.com>
2022-04-21 20:43:54 +0500

    first commit

---
+++ Four fuction calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Four_fuction_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1;
+           num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter an opretor: ");
+            string op = Console.ReadLine();
+            Console.Write("Enter the second number: ");
+            double num2;
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+        
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2); 
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2); 
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
